62b01bfbc52a950ce9461fc62cc04d6fc099b5bc
And_Ocean <and_ocean@163.com>
2024-08-01 11:28:22 +0800

    会议纪要

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("E:\\Exam-cheating-detection-system\\Exam-cheating-detection-system\\ultralytics-main\\yolov8n-pose.pt")
 
-results = model(source=0, show=True, conf=0.3, save=False)
+results = model(source=0, show=True, conf=0.3, save_txt=True)
