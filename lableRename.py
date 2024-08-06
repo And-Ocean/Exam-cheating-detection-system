d8b1c62beb0f970866019f913ff0310575d493e4
And_Ocean <and_ocean@163.com>
2024-08-06 15:08:18 +0800

    优化了标签重命名脚本
    可以通过终端指定异常行为所在帧区间了

--- lableRename.py
+++ lableRename.py
@@ -1,16 +1,32 @@
 import os
 
-def rename_files(data_dir, abnormal_start, abnormal_end):
+def get_abnormal_ranges():
+    abnormal_ranges = []
+    while True:
+        start = input("输入异常行为起始帧（或输入'q'结束）：")
+        if start.lower() == 'q':
+            break
+        end = input("输入异常行为结束帧：")
+        abnormal_ranges.append((int(start), int(end)))
+    return abnormal_ranges
+
+def is_abnormal_frame(frame_number, abnormal_ranges):
+    for start, end in abnormal_ranges:
+        if start <= frame_number <= end:
+            return True, frame_number - start + 1
+    return False, None
+
+def rename_files(data_dir, abnormal_ranges):
     for filename in os.listdir(data_dir):
-        if filename.endswith('.txt') and filename.startswith('test1_'):
+        if filename.endswith('.txt') and filename.startswith('test4_'):
             # 提取帧序号
             frame_number_str = filename.split('_')[1].split('.')[0]
             frame_number = int(frame_number_str)
             
             # 根据帧序号确定行为类型
-            if abnormal_start <= frame_number <= abnormal_end:
+            is_abnormal, abnormal_frame_number = is_abnormal_frame(frame_number, abnormal_ranges)
+            if is_abnormal:
                 # 异常行为帧
-                abnormal_frame_number = frame_number - abnormal_start + 1
                 new_filename = f'frame_{frame_number_str.zfill(4)}_abnormal_{str(abnormal_frame_number).zfill(4)}.txt'
             else:
                 # 正常行为帧
@@ -22,11 +38,11 @@ def rename_files(data_dir, abnormal_start, abnormal_end):
             os.rename(old_filepath, new_filepath)
             print(f'Renamed {filename} to {new_filename}')
 
+# 获取异常行为范围
+abnormal_ranges = get_abnormal_ranges()
+
 # 文件目录路径
-data_dir = 'runs\pose\predict2\labels'  # 替换为你的TXT文件目录
+data_dir = 'runs/pose/predict4/labels'
 
-# 异常行为帧范围
-abnormal_start = 240
-abnormal_end = 360
-
-rename_files(data_dir, abnormal_start, abnormal_end)
+# 重命名文件
+rename_files(data_dir, abnormal_ranges)
